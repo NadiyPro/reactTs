b96117931dae3fd9860ba2745eee7b8e5d1c1b0e
NadiyPro <siroviyn13@gmail.com>
2024-07-01 20:12:23 +0300

    create

--- src/component/userServise.ts
+++ src/component/userServise.ts
@@ -1,14 +1,16 @@
 import axios, {AxiosResponse} from "axios";
+import {IUsers} from "./IUsers";
+import {IPosts} from "./IPosts";
 
 let axiosUsers = axios.create({
     baseURL: 'https://dummyjson.com',
     headers: {'Content-Type':'application/json'}
 })
 
-const getAllUsers = async () => {
+const getAllUsers = async ():Promise<{users: IUsers[]}> => {
     return await axiosUsers.get('/users').then((response) => response.data)
 }
-const getUserPost = async (id:number) => {
+const getUserPost = async (id:number):Promise<{posts: IPosts[]}> => {
     return await axiosUsers.get('/users/' + id + '/posts').then((response) => response.data)
 }
 
