8389ee188abc19f255e5395f38f14476b6b5eb37
NadiyPro <siroviyn13@gmail.com>
2024-07-04 00:04:20 +0300

    create

--- src/component/Toggle.tsx
+++ src/component/Toggle.tsx
@@ -1,13 +1,14 @@
-import {HTMLProps, useState} from 'react';
+import {useState} from 'react';
 
 type ReturnValue = [() => void,string]
+
 export const useToggle = (init:boolean):ReturnValue => {
     const [toggle, setToggle] = useState<boolean>(init)
-    const [backgroundColor, setBackgroundColor] = useState<any>('cornflowerblue')
+    const [backgroundColor, setBackgroundColor] = useState<string>('cornflowerblue')
     const colorToggle = () => {
         setToggle(value => !value);
         setBackgroundColor(toggle? 'cornflowerblue' : 'rosybrown' )
   }
 
-    return [colorToggle,backgroundColor];
+    return [colorToggle, backgroundColor];
 }
