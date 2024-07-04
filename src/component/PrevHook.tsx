f6b4028dfcafd52c5d3bfc5c7fab6fd9c8299402
NadiyPro <siroviyn13@gmail.com>
2024-07-04 14:37:39 +0300

    create

---
+++ src/component/PrevHook.tsx
@@ -0,0 +1,11 @@
+import React, {useRef} from 'react';
+
+export const usePrevHook = (value:number) => {
+    const currentValue = useRef<number>(value)
+    const prevValue = useRef<number>()
+    if(currentValue.current !== value){
+        prevValue.current = currentValue.current;
+        currentValue.current = value;
+    }
+    return [currentValue.current, prevValue.current];
+};
