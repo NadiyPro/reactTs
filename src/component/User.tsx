6cbde7082c4d391db8ff3ce0ec4941852a8b98b2
NadiyPro <siroviyn13@gmail.com>
2024-07-06 13:04:26 +0300

    create

---
+++ src/component/User.tsx
@@ -0,0 +1,19 @@
+import React, {Component, FC} from "react";
+import {IUsers} from "../module/IUsers";
+import "./userPostStyle.css";
+
+interface PostState{
+    user:IUsers
+    getPost:(id:number) => void
+}
+
+class UserComponent extends Component<{}, PostState>{
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
--- src/component/Users.tsx
+++ src/component/Users.tsx
@@ -5,21 +5,24 @@ import {getAllUsers, getUserPost} from "../servise/userServise";
 import {IPosts} from "../module/IPosts";
 type UsersState = {
     users:IUsers[]
+    posts:IPosts[]
 }
-
 class UsersComponents extends Component<{}, UsersState>{
     state: UsersState = {
-        users: []
+        users: [],
+        posts: []
 }
-
     componentDidMount() {
-        getAllUsers().then(value => value.users)
+        getAllUsers().then(value => this.state.users)
     }
+    // componentDidMount(id:number) {
+    //     getUserPost(id).then(value => this.state.posts)
+    // }
 
     render() {
         return (
             <div>
-
+                {this.state.users.map(user=> (<div> {user.id} {user.firstName} {user.lastName} {user.maidenName} age - {user.age}</div>))}
             </div>
         )
     }
