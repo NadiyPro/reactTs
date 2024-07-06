1e813408a420b77d3e96f1d840d2e5d8fe894dab
NadiyPro <siroviyn13@gmail.com>
2024-07-06 15:07:56 +0300

    create

--- src/component/User.tsx
+++ src/component/User.tsx
@@ -1,25 +1,17 @@
 import React, {Component} from "react";
-import { IPosts } from "../module/IPosts";
 import {IUsers} from "../module/IUsers";
-import {getUserPost} from "../servise/userServise";
 
 interface UserProps{
     user:IUsers
 }
-interface UserState{
-    getPost: (id:number) => void
-    posts: IPosts []
-}
-class UserComponent extends Component<UserProps, UserState>{
+class UserComponent extends Component<UserProps, { }>{
 
-    componentDidMount() {
-        getUserPost(this.props.user.id).then(value => this.setState({posts: value.posts}))
-    }
+
     render() {
         return (<div>
             {this.props.user.id} {this.props.user.firstName} {this.props.user.lastName} {this.props.user.maidenName} age - {this.props.user.age}
             <button onClick={() => {
-                this.state.getPost(this.props.user.id)
+
             }}>Click
             </button>
         </div>);
