a6fbe8ff9b72743c31809d3b75326c985d4ced0c
NadiyPro <siroviyn13@gmail.com>
2024-07-06 12:33:14 +0300

    create

--- src/component/Users.tsx
+++ src/component/Users.tsx
@@ -1,39 +1,26 @@
-import React, {FC, useEffect, useState} from 'react';
+import React, {Component, FC, useEffect, useState} from 'react';
 import {IUsers} from "../module/IUsers";
 import {getAllUsers, getUserPost} from "../servise/userServise";
 // import UserComponent from "./User";
 import {IPosts} from "../module/IPosts";
+type UsersState = {
+    users:IUsers[]
+}
 
+class UsersComponents extends Component<{}, UsersState>{
+    state: UsersState = {
+        users: []
+}
 
+    componentDidMount() {
+        getAllUsers().then(value => value.users)
+    }
 
-const UsersComponents:FC = () => {
-    const [users,setUsers] = useState<IUsers[]>([]);
-
-    useEffect(() => {
-        getAllUsers()
-            .then(value  => setUsers(value.users))
-    }, []);
-    const [posts,setPosts] = useState<IPosts[]>([]);
-    const getPost =  (id:number) => {getUserPost(id).then(value =>setPosts(value.posts))}
-
-    return (
-        <div>
-            <hr/>
+    render() {
+        return (
             <div>
-                {
-                    // users.map(user => <UserComponent
-                    //     key={user.id} user={user} getPost={getPost}
-                    // />)
-                }
-            </div>
-            <hr/>
-            <div>
-                {
-                    posts.map(post => <div key={post.id}><h2>{post.title}</h2> <p> userId - {post.userId} id post - {post.id} </p> <p>body - {post.body}</p></div>)
-                }
-            </div>
-        </div>
-    );
-};
 
-export default UsersComponents;
+            </div>
+        )
+    }
+}
