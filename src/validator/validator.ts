48a27570ac6d3640a7c9130db40205828dae348b
NadiyPro <siroviyn13@gmail.com>
2024-07-08 19:12:53 +0300

    create

--- src/validator/validator.ts
+++ src/validator/validator.ts
@@ -2,22 +2,22 @@ import Joi from "joi";
 
 const validatorJoi = Joi.object({
     userId:Joi.number().min(1).max(117).required().messages({
-        "number.min":"min is 1",
-        "number.max":"max is 117"
+        "number.min":"min is 1 number",
+        "number.max":"max is 117 number"
     }),
     id:Joi.number().min(1).max(117).required().messages({
-        "number.min":"min is 1",
-        "number.max":"max is 117"
+        "number.min":"min is 1 number",
+        "number.max":"max is 117 number"
     }),
     title:Joi.string().min(5).max(250).required().messages({
         "string.empty.base":"only chars allowed",
         "string.min":"min is 5 allowed",
-        "string.max.base":"max is 117"
+        "string.max.base":"max is 117 allowed"
     }),
     body:Joi.string().min(5).max(250).required().messages({
         "string.empty.base":"only chars allowed",
         "string.min":"min is 5 allowed",
-        "string.max.base":"max is 117"
+        "string.max.base":"max is 117 allowed"
     })
 })
 
