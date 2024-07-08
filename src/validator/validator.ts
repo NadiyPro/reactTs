80db71582e4e68f244f806a65634254079c4964e
NadiyPro <siroviyn13@gmail.com>
2024-07-08 20:12:51 +0300

    create

--- src/validator/validator.ts
+++ src/validator/validator.ts
@@ -6,14 +6,14 @@ const validatorJoi = Joi.object({
         "number.max":"max is 117 number"
     }),
     title:Joi.string().min(5).max(250).required().messages({
-        "string.empty.base":"only chars allowed",
-        "string.min.base":"min is 5 allowed",
-        "string.max.base":"max is 117 allowed"
+        "string.empty":"only chars allowed",
+        "string.min":"min is 5 allowed",
+        "string.max":"max is 117 allowed"
     }),
     body:Joi.string().min(5).max(250).required().messages({
-        "string.empty.base":"only chars allowed",
-        "string.min.base":"min is 5 allowed",
-        "string.max.base":"max is 117 allowed"
+        "string.empty":"only chars allowed",
+        "string.min":"min is 5 allowed",
+        "string.max":"max is 117 allowed"
     })
 })
 
