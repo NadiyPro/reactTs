83ab92a63ac6242d7ca45c50c261e7842d810ec2
NadiyPro <siroviyn13@gmail.com>
2024-07-08 18:39:58 +0300

    create

--- src/component/FormComponent.tsx
+++ src/component/FormComponent.tsx
@@ -5,33 +5,36 @@ import {joiResolver} from "@hookform/resolvers/joi";
 import validatorJoi from "../validator/validator";
 
 const FormComponent = () => {
-    const {handleSubmit,register, formState: {errors, isValid}} = useForm<IPosts>({mode:'all', resolver:joiResolver(validatorJoi)})
+    const {handleSubmit,register, formState: {errors, isValid}} =
+        useForm<IPosts>({mode:'all', resolver: joiResolver(validatorJoi)})
     const registerHandle = (value:IPosts) => {
         console.log(value)
     }
     return (
-        <div>
-            <form onClick={handleSubmit(registerHandle)}>
+        <div style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', flexDirection: "column"}}>
+            <form onClick={handleSubmit(registerHandle)}
+                  style={{height: '40vh', width: '10vw',display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', flexDirection: "column"}}>
                 <label>
                     Please write your user id
-                    <input {...register('userId')}/>
+                    <input type={'number'} {...register('userId')}/>
                     {errors.userId && <div>{errors.userId.message}</div>}
                 </label>
                 <label>
-                    <input {...register('id')}/>
+                    Please enter your post number
+                    <input type={'number'} {...register('id')}/>
                     {errors.id && <div>{errors.id.message}</div>}
                 </label>
                 <label>
                     Please write the title of the post
-                    <input {...register('title')}/>
+                    <input type={'text'} {...register('title')}/>
                     {errors.title && <div>{errors.title.message}</div>}
                 </label>
                 <label>
                     Please write your post
-                    <input {...register('body')}/>
+                    <input type={'text'} {...register('body')}/>
                     {errors.body && <div>{errors.body.message}</div>}
                 </label>
-                    <button disabled={!isValid}>Click</button>
+                <button disabled={!isValid}>Click</button>
             </form>
         </div>
     );
--- src/validator/validator.ts
+++ src/validator/validator.ts
@@ -1,19 +1,19 @@
 import Joi from "joi";
 
 const validatorJoi = Joi.object({
-    userId:Joi.string().required().min(1).max(117).messages({
-        "string.min":"min is 1",
-        "string.max":"max is 117"
+    userId:Joi.string().min(1).max(117).required().messages({
+        'string.min.base':'min is 1',
+        'string.max.base':'max is 117'
     }),
-    id:Joi.string().required().min(1).max(117).messages({
-        "string.min":"min is 1",
-        "string.max":"max is 117"
+    id:Joi.string().min(1).max(117).required().messages({
+        "string.min.base":"min is 1",
+        "string.max.base":"max is 117"
     }),
-    title:Joi.string().required().pattern(/w/s).messages({
-        "string.pattern.base":"only chars allowed"
+    title:Joi.string().required().messages({
+        "string.empty":"only chars allowed"
     }),
-    body:Joi.string().required().pattern(/w/s).messages({
-        "string.pattern.base":"only chars allowed"
+    body:Joi.string().required().messages({
+        "string.empty":"only chars allowed"
     })
 })
 
