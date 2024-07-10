56ddb3eff2cd9122365ef9940e8b26e2f2661d76
NadiyPro <siroviyn13@gmail.com>
2024-07-10 15:54:31 +0300

    create

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const browserRouter = createBrowserRouter([])
 root.render(
-    <App />
+    <RouterProvider router={browserRouter} />
 );
 
 reportWebVitals();
