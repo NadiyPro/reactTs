e5a21dfda6bd580b853c07e687c4444c9f385a71
NadiyPro <siroviyn13@gmail.com>
2024-07-10 21:40:09 +0300

    create

--- src/component/Menu.tsx
+++ src/component/Menu.tsx
@@ -7,7 +7,6 @@ const Menu = () => {
             <li><NavLink to={'users'}>users</NavLink></li>
             <li><NavLink to={'posts'}>posts</NavLink></li>
             <li><NavLink to={'comments'}>comments</NavLink></li>
-            <li><NavLink to={'back_menu'}>back menu</NavLink></li>
         </ul>
     );
 };
