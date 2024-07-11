d2981bbe49738dbc8710e14f76ba7eb3d1cea5d5
NadiyPro <siroviyn13@gmail.com>
2024-07-11 23:40:12 +0300

    create

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useParams} from "react-router-dom";
 
 const Post = () => {
+    let {id} = useParams();
+
+
     return (
         <div>
 
