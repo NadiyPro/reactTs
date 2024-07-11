8c484e2bfd1c026866a599a57a91e582708441dd
NadiyPro <siroviyn13@gmail.com>
2024-07-12 00:54:44 +0300

    create

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -3,19 +3,22 @@ import {useLocation, useParams} from "react-router-dom";
 import {getUserPost} from "../servise/usersService";
 import {IPosts} from "../module/IPosts";
 import {IUsers} from "../module/IUsers";
+import posts from "./Posts";
 
 const Post = () => {
     let {id} = useParams();
     let location= useLocation();
-    let state: IPosts = location.state
+    let state = location.state
     let [post, setPost] = useState<IPosts | null>(null)
     useEffect(() => {
-        getUserPost(state.id + '').then(value => console.log(value))
-    }, []);
+        getUserPost(state.id + '').then((value:IPosts[]) => {setPost(value)})
+    }, [id]);
 
     return (
         <div>
-           htht
+            {
+                post?.title
+            }
         </div>
     );
 };
