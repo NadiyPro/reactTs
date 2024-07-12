d5ea2019f9fbe60a65d38a8bcad53555f7a17e1f
NadiyPro <siroviyn13@gmail.com>
2024-07-12 09:08:22 +0300

    create

---
+++ src/component/Post.tsx
@@ -0,0 +1,17 @@
+import React, {FC} from 'react';
+import {IPosts} from "../module/IPosts";
+
+
+interface IPost{
+    post:IPosts
+}
+const Post:FC<IPost> = ({post}) => {
+    return (
+        <div>
+            <div key={post.id}><h2>{post.title}</h2> <p> userId - {post.userId} id post - {post.id} </p> <p>body
+                - {post.body}</p></div>
+        </div>
+    );
+};
+
+export default Post;
--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {getAllPosts} from "../servise/usersService";
 import {IPosts} from "../module/IPosts";
+import Post from "../component/Post";
 
 const Posts = () => {
     const [posts,setPosts] = useState<IPosts[]>([]);
@@ -13,7 +14,7 @@ const Posts = () => {
     return (
         <div>
             {
-                posts && posts.map(post => <div key={post.id}><h2>{post.title}</h2> <p> userId - {post.userId} id post - {post.id} </p> <p>body - {post.body}</p></div>)
+               posts.map(post => <Post post={post}/>)
             }
         </div>
     );
