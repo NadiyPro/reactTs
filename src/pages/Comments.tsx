04a56fe2ddce58a846f9603766902bcd8610a812
NadiyPro <siroviyn13@gmail.com>
2024-07-12 08:47:36 +0300

    create

--- src/pages/Comments.tsx
+++ src/pages/Comments.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { getPostComments} from "../servise/usersService";
+import {IComments} from "../module/IComments";
 
 const Comments = () => {
+    const [comments,setComments] = useState<IComments[]>([]);
+
+    useEffect(() => {
+        getPostComments().then(value => {
+            setComments(value)
+        })
+    }, []);
     return (
         <div>
-            
+            {comments.map(comment => <div key={comment.id}><h4>postId: {comment.postId}</h4>id: {comment.id} <br/>
+                name: {comment.name} <br/> email: {comment.email} <br/> body: {comment.body} </div>)}
         </div>
     );
 };
