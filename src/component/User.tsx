131424c776c3b401a5ca20933a902932b3c6058e
NadiyPro <siroviyn13@gmail.com>
2024-07-13 08:42:47 +0300

    create

--- src/component/User.tsx
+++ src/component/User.tsx
@@ -1,15 +1,18 @@
 import React, {FC} from 'react';
 import {IUsers} from "../module/IUsers";
+import {useNavigate} from "react-router-dom";
 
 interface IUser{
     user:IUsers
 }
 const User:FC<IUser> = ({user}) => {
-
+let navigate = useNavigate();
     return (
         <div>
             <div><h4> id:{user.id} </h4> <p> name: {user.name}
                 <br/> username: {user.username} <br/> email: {user.email}<br/></p>
+
+                <button onClick={() => navigate(user.id.toString() + '/posts')}>post details</button>
 
                 <p>address: <br/> suite:{user.address.suite} <br/> city:{user.address.city}
                     <br/>zipcode:{user.address.zipcode} <br/> geo: <br/> lat:{user.address.geo.lat}
