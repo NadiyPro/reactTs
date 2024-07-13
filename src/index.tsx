bd6361097202849c34e1e60e777d09c49f953713
NadiyPro <siroviyn13@gmail.com>
2024-07-13 09:29:00 +0300

    create

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import Comments from "./pages/Comments";
 import Posts from "./pages/Posts";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 
 const browserRouter = createBrowserRouter([
