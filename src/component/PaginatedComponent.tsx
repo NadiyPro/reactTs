ebfa2407e216ded1de33fa889efb47dc07a1c74a
NadiyPro <siroviyn13@gmail.com>
2024-07-31 21:10:53 +0300

    create

--- src/component/PaginatedComponent.tsx
+++ src/component/PaginatedComponent.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {FC} from 'react';
+import {IPaginatedModel} from "../module/IPaginatedModel";
+import {useSearchParams} from "react-router-dom";
 
-
-const PaginatedComponent = () => {
+interface IPropsPaginated{
+    next: null | IPaginatedModel,
+    prev: null | IPaginatedModel
+}
+const PaginatedComponent:FC<IPropsPaginated> = ({next,prev}) => {
+    const [query, setQuery] = useSearchParams({page: '1'});
+    const changePage = (nextPrev:string) => {
+        switch (nextPrev) {
+            case 'next':
+                setQuery({...next});
+                break;
+            case 'prev':
+                setQuery({...prev});
+                break;
+        }
+    }
     return (
         <div>
-
+            <button disabled={!next} onClick={() => {changePage('next')}}>next</button>
+            <button disabled={!prev} onClick={() => {changePage('prev')}}>prev</button>
         </div>
     );
 };
--- src/module/IPaginatedModel.ts
+++ src/module/IPaginatedModel.ts
@@ -1,3 +1,3 @@
 export interface IPaginatedModel{
-    page:string
+    page: string
 }
