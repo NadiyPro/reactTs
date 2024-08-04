c00bc68937d9e7b5b732a5c8227b3a196b887ede
NadiyPro <siroviyn13@gmail.com>
2024-08-04 20:12:12 +0300

    create

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React, {FC} from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
+import {useAppDispatch, useAppSelector } from "./redux/store";
+import {userActions} from "./redux/slices/userSlice";
 
-const App:FC = () => {
-  return (
-      <>
+const App = () => {
 
-      </>
-  );
+    let {userSlice: {users,isLoaded}} = useAppSelector(state => state);
+
+    let dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(userActions.loadUsers());
+    }, []);
+
+
+    return (
+        <div>
+            {!isLoaded && <div>Loading in process....</div>}
+
+            {users.map(user => <div>{user.name}</div>)}
+        </div>
+    );
 }
+
 export default App;
