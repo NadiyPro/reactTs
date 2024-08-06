261d5d0d82a78a31ad30dbc06ca61c79e5ac6e7d
NadiyPro <siroviyn13@gmail.com>
2024-08-06 06:59:50 +0300

    create

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -11,21 +11,21 @@ const axiosInstance = axios.create({
 
 
 export const userService = {
-    getAll: async () => {
+    getAll: async ():Promise<IUser[]>=> {
         const response = await axiosInstance.get<IUser[]>(urls.users.base);
         return response.data;
     }
 }
 
 export const postService = {
-    getAll: async () => {
+    getAll: async ():Promise<IPost[]> => {
         let response = await axiosInstance.get<IPost[]>(urls.posts.base);
         return response.data;
     }
 }
 
 export const commentsService = {
-    getAll: async () => {
+    getAll: async ():Promise<IComment[]> => {
         let response = await axiosInstance.get<IComment[]>(urls.comments.base);
         return response.data;
     }
